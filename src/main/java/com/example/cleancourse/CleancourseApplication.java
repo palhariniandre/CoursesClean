2f1670852b9dfed654319811e8e77908ddb1d635
dede <warder63@gmail.com>
2026-04-16 18:18:03 -0300

    feat01: dependencies and structure add

---
+++ src/main/java/com/example/cleancourse/CleancourseApplication.java
@@ -0,0 +1,13 @@
+package com.example.cleancourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleancourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleancourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cleancourse/CleancourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cleancourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleancourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
